8a742448b45f15a15077b3619b447772dfc693ee
xaditya <xaditya242@gmail.com>
2026-07-20 21:36:10 +0700

    fix type data humidity

--- app/src/main/java/com/example/hics/HomeFragment.kt
+++ app/src/main/java/com/example/hics/HomeFragment.kt
@@ -39,7 +39,7 @@ class HomeFragment : Fragment() {
     var suhuAir = 0.0
     var pH = 0.0
     var nutrisi = 0
-    var humidity = 0
+    var humidity = 0.0
     var level = 0.0
     var isOn = true
 
@@ -111,7 +111,7 @@ class HomeFragment : Fragment() {
 
                 notifAlert = when (notifValue) {
                     is Boolean -> notifValue
-                    is String -> notifValue.toBoolean()
+                    is String  -> notifValue.toBoolean()
                     else -> false
                 }
 
@@ -123,7 +123,7 @@ class HomeFragment : Fragment() {
                 pH = snapshot.child("dataStream/ph").value.toString().toDoubleOrNull() ?: 0.0
                 nutrisi = snapshot.child("dataStream/ppm").value.toString().toIntOrNull() ?: 0
                 level = snapshot.child("dataStream/waterLevel").value.toString().toDoubleOrNull() ?: 0.0
-                humidity = snapshot.child("dataStream/humidity").value.toString().toIntOrNull() ?: 0
+                humidity = snapshot.child("dataStream/humidity").value.toString().toDoubleOrNull() ?: 0.0
                 isOn = snapshot.child("control/waterPump").value.toString().toBoolean()
 
                 // ===== NOTIF =====
@@ -140,7 +140,7 @@ class HomeFragment : Fragment() {
                 // ===== UI =====
                 phTextView.text = String.format("%.1f", pH)
                 nutrisiTextView.text = nutrisi.toString()
-                humidityText.text = "$humidity%"
+                humidityText.text = "%.2f%%".format(humidity)
                 statusSwitch.text = if (isOn) "ON" else "OFF"
 
                 var displayAirTemp = suhuUdara
